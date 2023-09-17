f435455652c2774a821d78c2eef69d49f5d2039c
duongdinhthu <143236113+duongdinhthu@users.noreply.github.com>
2023-09-17 17:17:14 +0700

    up

---
+++ tuviet/formlogin.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    // $ là tạo sự kiện cho document, ready là sẵn sàng chạy 
+    $('#eye').click(function(){
+        // $ tạo sự kiện khi click vào con mắt trên wed 
+        $(this).toggleClass('open')
+        $(this).children('i').toggleClass('fa-eye-slash fa-eye');
+        // khi click vào con mắt thì sẽ gọi i và thay đổi toogleClas fa-eye-slash thành fa-eye
+        if($(this).hasClass('open')){
+            // nếu sự kiện click con mắt có open thì ...
+            $(this).prev().attr('type', 'text');
+            // từ i gọi đến thằng input bên trên , tiếp tục gọi đến attr và gán thay type(hiện đang là password sẽ chuyển sang text)             
+        }
+        else {
+            $(this).prev().attr('type', 'password');
+        }
+    });
+
+});
