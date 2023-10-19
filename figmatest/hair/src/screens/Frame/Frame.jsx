1eed2eb2b98d94293d24dff8ac10103e5564990f
duongdinhthu <143236113+duongdinhthu@users.noreply.github.com>
2023-10-19 23:56:24 +0700

    up

---
+++ figmatest/hair/src/screens/Frame/Frame.jsx
@@ -0,0 +1,248 @@
+import React from "react";
+import "./style.css";
+
+export const Frame = () => {
+  return (
+    <div className="frame">
+      <div className="div">
+        <div className="div-2">
+          <div className="overlap-group">
+            <div className="text-wrapper">Chai xịt</div>
+          </div>
+          <div className="div-3">
+            <p className="SALE">
+              <span className="span">
+                SALE 50%
+                <br />
+                <br />
+              </span>
+              <span className="text-wrapper-2">1.000.000.000Đ&nbsp;&nbsp;&nbsp;&nbsp; </span>
+            </p>
+            <img className="img" alt="Download" src="/img/download-4.png" />
+          </div>
+          <div className="div-4">
+            <p className="SALE">
+              <span className="span">
+                SALE 50%
+                <br />
+                <br />
+              </span>
+              <span className="text-wrapper-2">1.000.000.000Đ&nbsp;&nbsp;&nbsp;&nbsp; </span>
+            </p>
+            <img className="img" alt="Download" src="/img/download-6.png" />
+          </div>
+          <div className="div-5">
+            <p className="SALE">
+              <span className="span">
+                SALE 50%
+                <br />
+                <br />
+              </span>
+              <span className="text-wrapper-2">1.000.000.000Đ&nbsp;&nbsp;&nbsp;&nbsp; </span>
+            </p>
+            <img className="img" alt="Images" src="/img/images-26-1.png" />
+          </div>
+        </div>
+        <div className="div-6">
+          <div className="overlap-group">
+            <div className="text-wrapper">Máy hấp</div>
+          </div>
+          <div className="div-3">
+            <p className="SALE">
+              <span className="span">
+                SALE 50%
+                <br />
+                <br />
+              </span>
+              <span className="text-wrapper-2">1.000.000.000Đ&nbsp;&nbsp;&nbsp;&nbsp; </span>
+            </p>
+            <img className="img" alt="Images" src="/img/images-30.png" />
+          </div>
+          <div className="div-4">
+            <p className="SALE">
+              <span className="span">
+                SALE 50%
+                <br />
+                <br />
+              </span>
+              <span className="text-wrapper-2">1.000.000.000Đ&nbsp;&nbsp;&nbsp;&nbsp; </span>
+            </p>
+            <img className="download" alt="Download" src="/img/download-7.png" />
+          </div>
+          <div className="div-5">
+            <p className="SALE">
+              <span className="span">
+                SALE 50%
+                <br />
+                <br />
+              </span>
+              <span className="text-wrapper-2">1.000.000.000Đ&nbsp;&nbsp;&nbsp;&nbsp; </span>
+            </p>
+            <img className="img" alt="Download" src="/img/download-8.png" />
+          </div>
+        </div>
+        <div className="div-7">
+          <div className="overlap-group">
+            <div className="text-wrapper">Máy mát xa</div>
+          </div>
+          <div className="div-3">
+            <p className="SALE">
+              <span className="span">
+                SALE 50%
+                <br />
+                <br />
+              </span>
+              <span className="text-wrapper-2">1.000.000.000Đ&nbsp;&nbsp;&nbsp;&nbsp; </span>
+            </p>
+            <img className="img" alt="Images" src="/img/images-31.png" />
+          </div>
+          <div className="div-4">
+            <p className="SALE">
+              <span className="span">
+                SALE 50%
+                <br />
+                <br />
+              </span>
+              <span className="text-wrapper-2">1.000.000.000Đ&nbsp;&nbsp;&nbsp;&nbsp; </span>
+            </p>
+            <img className="img" alt="Images" src="/img/images-32.png" />
+          </div>
+          <div className="div-5">
+            <p className="SALE">
+              <span className="span">
+                SALE 50%
+                <br />
+                <br />
+              </span>
+              <span className="text-wrapper-2">1.000.000.000Đ&nbsp;&nbsp;&nbsp;&nbsp; </span>
+            </p>
+            <img className="img" alt="Images" src="/img/images-33.png" />
+          </div>
+        </div>
+        <div className="div-8">
+          <div className="overlap-group">
+            <div className="text-wrapper">Mũ spa</div>
+          </div>
+          <div className="div-3">
+            <p className="SALE">
+              <span className="span">
+                SALE 50%
+                <br />
+                <br />
+              </span>
+              <span className="text-wrapper-2">1.000.000.000Đ&nbsp;&nbsp;&nbsp;&nbsp; </span>
+            </p>
+            <img className="img" alt="Images" src="/img/images-19.png" />
+          </div>
+          <div className="div-4">
+            <p className="SALE">
+              <span className="span">
+                SALE 50%
+                <br />
+                <br />
+              </span>
+              <span className="text-wrapper-2">1.000.000.000Đ&nbsp;&nbsp;&nbsp;&nbsp; </span>
+            </p>
+            <img className="img" alt="Images" src="/img/images-20.png" />
+          </div>
+          <div className="div-5">
+            <p className="SALE">
+              <span className="span">
+                SALE 50%
+                <br />
+                <br />
+              </span>
+              <span className="text-wrapper-2">1.000.000.000Đ&nbsp;&nbsp;&nbsp;&nbsp; </span>
+            </p>
+            <img className="img" alt="Images" src="/img/images-21.png" />
+          </div>
+        </div>
+        <div className="text-wrapper-3">Liên hệ</div>
+        <div className="text-wrapper-4">Giới thiệu</div>
+        <div className="text-wrapper-5">Dịch vụ</div>
+        <div className="text-wrapper-6">Sản phẩm</div>
+        <div className="text-wrapper-7">Trang chủ</div>
+        <div className="overlap">
+          <p className="p">Vì một mái tóc không hói</p>
+          <img className="images" alt="Images" src="/img/images-3.png" />
+        </div>
+        <div className="div-9">
+          <div className="overlap-group">
+            <div className="text-wrapper">Máy sấy</div>
+          </div>
+          <div className="div-3">
+            <p className="SALE">
+              <span className="span">
+                SALE 50%
+                <br />
+                <br />
+              </span>
+              <span className="text-wrapper-2">1.000.000.000Đ&nbsp;&nbsp;&nbsp;&nbsp; </span>
+            </p>
+            <img className="img" alt="Images" src="/img/images-15.png" />
+          </div>
+          <div className="div-4">
+            <p className="SALE">
+              <span className="span">
+                SALE 50%
+                <br />
+                <br />
+              </span>
+              <span className="text-wrapper-2">1.000.000.000Đ&nbsp;&nbsp;&nbsp;&nbsp; </span>
+            </p>
+            <img className="img" alt="Images" src="/img/images-12.png" />
+          </div>
+          <div className="div-10">
+            <p className="SALE">
+              <span className="span">
+                SALE 50%
+                <br />
+                <br />
+              </span>
+              <span className="text-wrapper-2">1.000.000.000Đ&nbsp;&nbsp;&nbsp;&nbsp; </span>
+            </p>
+            <img className="img" alt="Download" src="/img/download-3.png" />
+          </div>
+        </div>
+        <div className="div-11">
+          <div className="overlap-group">
+            <div className="text-wrapper">Máy ép tóc</div>
+          </div>
+          <div className="div-3">
+            <img className="img" alt="Images" src="/img/images-13.png" />
+            <p className="SALE">
+              <span className="span">
+                SALE 50%
+                <br />
+                <br />
+              </span>
+              <span className="text-wrapper-2">1.000.000.000Đ&nbsp;&nbsp;&nbsp;&nbsp; </span>
+            </p>
+          </div>
+          <div className="div-4">
+            <p className="SALE">
+              <span className="span">
+                SALE 50%
+                <br />
+                <br />
+              </span>
+              <span className="text-wrapper-2">1.000.000.000Đ&nbsp;&nbsp;&nbsp;&nbsp; </span>
+            </p>
+            <img className="img" alt="Images" src="/img/images-26.png" />
+          </div>
+          <div className="div-5">
+            <p className="SALE">
+              <span className="span">
+                SALE 50%
+                <br />
+                <br />
+              </span>
+              <span className="text-wrapper-2">1.000.000.000Đ&nbsp;&nbsp;&nbsp;&nbsp; </span>
+            </p>
+            <img className="img" alt="Images" src="/img/images-27.png" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ figmatest/hair2/src/screens/Frame/Frame.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./style.css";
+
+export const Frame = () => {
+  return (
+    <div className="frame">
+      <div className="div">
+        <div className="text-wrapper">Liên hệ</div>
+        <div className="text-wrapper-2">Before</div>
+        <div className="text-wrapper-3">After</div>
+        <img className="havtympvpqzablpc" alt="Havtympvpqzablpc" src="/img/havtympvpqz33ablp0c4-scaled-1.png" />
+        <div className="text-wrapper-4">Giới thiệu</div>
+        <div className="text-wrapper-5">Dịch vụ</div>
+        <div className="text-wrapper-6">Sản phẩm</div>
+        <div className="text-wrapper-7">Trang chủ</div>
+        <p className="p">Vì một mái tóc không hói</p>
+        <img className="images" alt="Images" src="/img/images-3.png" />
+        <img className="photo" alt="Photo" src="/img/photo1678350535182-16783505353201324819679-1.png" />
+        <img className="image" alt="Image" src="/img/image-1.png" />
+        <img className="img" alt="Image" src="/img/image-2.png" />
+        <div className="text-wrapper-8">Beauty Hair</div>
+        <p className="text-wrapper-9">Chuyên kinh doanh các sản phẩm chăm sóc tóc</p>
+        <div className="overlap-group">
+          <div className="text-wrapper-10">Giới thiệu</div>
+        </div>
+      </div>
+    </div>
+  );
+};
