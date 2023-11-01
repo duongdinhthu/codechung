9ada115035efe934561ab5f86caec115b0f2bbd2
duongdinhthu <143236113+duongdinhthu@users.noreply.github.com>
2023-11-01 23:06:18 +0700

    UP

---
+++ tuviet/3dworld.js
@@ -0,0 +1,16 @@
+$("#data a").click(function(e) {
+    var ref = $(this).attr("class");
+    solarsys.removeClass().addClass(ref);
+    $(this).parent().find('a').removeClass('active');
+    $(this).addClass('active');
+    e.preventDefault();
+  });
+
+  $(".set-view").click(function() { body.toggleClass("view-3D view-2D"); });
+  $(".set-zoom").click(function() { body.toggleClass("zoom-large zoom-close"); });
+  $(".set-speed").click(function() { setView("scale-stretched set-speed"); });
+  $(".set-size").click(function() { setView("scale-s set-size"); });
+  $(".set-distance").click(function() { setView("scale-d set-distance"); });
+
+  init();
+
