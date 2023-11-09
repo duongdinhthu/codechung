729dfd5a671f8760317bcc47ed90bee752c7aa9d
duongdinhthu <143236113+duongdinhthu@users.noreply.github.com>
2023-11-09 12:08:14 +0700

    up

--- duongthu/src/App.js
+++ duongthu/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    Brow
+    <h1>Hello world!</h1>
   );
 }
 
---
+++ duongthu/src/components/Login.js
@@ -0,0 +1,21 @@
+import React,{Component} from "react";
+import './style.css';
+//extends : kế thừa
+//vd: a extends b : thì B là lớp(class) cha của a
+class Login extends Component// test sẽ kế thừa lại những methods(phương thức) , properties(thuộc tính) trong lớp Component
+{
+    //render() xuất bản ra HTML => view
+    render(){
+        return(
+            <div>
+                <h2>Login Form</h2>
+                User Name: <input type="text"></input>
+                <br/>
+                Password: <input type="password"></input>
+                <br/>
+                <input type="submit"></input>
+            </div>
+        );
+    }  
+}
+export default Login;
