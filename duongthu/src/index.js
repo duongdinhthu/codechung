83ab7a6c0f983eac13d2ff1ae4b5daff0a149eb2
duongdinhthu <143236113+duongdinhthu@users.noreply.github.com>
2023-11-12 09:56:29 +0700

    up

--- duongthu/src/index.js
+++ duongthu/src/index.js
@@ -1,18 +1,47 @@
-import React from 'react';
+import React, {Component} from 'react';
+// import { ReactPropTypes } from 'react;
+// import PropTypes from 'prop-types'
+import PropTypes from 'prop-types'
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Login from './components/Login';
-
+class ComponentExample extends React.Component{
+render(){
+return(
+<div>
+{/* {print all props} */}
+<h1>
+{this.props.arrayProp}<br/>
+{this.props.stringProp}<br/>
+{this.props.numberProp}<br/>
+{this.props.boolProp}<br/>
+</h1>
+</div>
+);
+}
+}
+// componentClassName.propTypes: import kiểu(dữ liệu) của các property
+ComponentExample.propTypes={
+arrayProp:PropTypes.array,
+stringProp:PropTypes.string,
+numberProp:PropTypes.number,
+boolProp:PropTypes.bool,
+}
+ComponentExample.defaultProps ={
+arrayProp:['Ngoc', 'Binh', 'Thanh'],
+stringProp:"fpt-aptech",
+numberProp:20,
+boolProp:true,
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  
-  </React.StrictMode>
+<React.StrictMode>
+<ComponentExample></ComponentExample>
+{/* <App />  */}
+{/* <Login></Login> */}
+</React.StrictMode>
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
